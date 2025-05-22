ec562f4272d6d1cf9d647e07069f92c4807c73a1
Dimas Dharmawan <marswan27@gmail.com>
2025-05-22 22:25:21 +0700

    Mengubah favicon dan title halaman

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -10,10 +10,10 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'TestCase-Humma';
 
 createInertiaApp({
-    title: (title) => `${title} ${appName}`,
+    title: (title) => title || 'TestCase-Humma',
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.jsx`,
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'TestCase-Humma') }}</title>
+        <link rel="icon" type="image/svg+xml" href="{{ asset('images/notepad 1.svg') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
